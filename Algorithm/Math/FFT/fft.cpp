5538fd8725aceec95525d95c0c0d7e6043f16808
LBWei <0800680274united@gmail.com>
2023-02-06 12:06:28 +0800

    FFT/NTT

---
+++ Algorithm/Math/FFT/fft.cpp
@@ -0,0 +1,115 @@
+/*
+***Math*** -- Fast Fourier Transformation (FFT)
+    Note:
+        這裡的應用是大數乘法。
+    Time Complexity:
+        transformation: O(nlogn)
+        multiplication: O(n) (point-wise)
+    Reference:
+        https://blog.csdn.net/acdreamers/article/details/39005227
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+const ld PI = acos(-1.0);
+
+string s1, s2; // two large numbers
+
+struct Complex {
+    ld rel, img;
+    Complex(ld __r = 0.0, ld __i = 0.0): rel(__r), img(__i) {}
+    Complex operator+(const Complex &c) {
+        return Complex(rel + c.rel, img + c.img);
+    }
+    Complex operator-(const Complex &c) {
+        return Complex(rel - c.rel, img - c.img);
+    }
+    Complex operator*(const Complex &c) {
+        return Complex(rel*c.rel - img*c.img, rel*c.img + img*c.rel);
+    }
+};
+
+// change some order, making FFT easier to implement
+void rader(vector<Complex> &v) {
+    int len = v.size();
+    int j = len >> 1;
+    for (int i=1; i<len-1; ++i) {
+        if (i < j) swap(v[i], v[j]);
+        int k = len >> 1;
+        while (j >= k) {
+            j -= k;
+            k >>= 1;
+        }
+        if (j < k) j += k;
+    }
+}
+
+// If inv = -1: inverse FFT
+void FFT (vector<Complex> &F, int inv) {
+    int len = F.size();
+    rader(F);
+    for (int seg=2; seg<=len; seg<<=1) {
+        Complex omgn(cos(-inv * 2 * PI / seg), sin(-inv * 2 * PI / seg));
+        for (int j=0; j<len; j+=seg) {
+            Complex omg(1.0, 0.0);
+            for (int k=j; k < j+seg/2; ++k) {
+                Complex x = F[k];
+                Complex y = omg * F[k + seg / 2];
+                F[k] = x + y;
+                F[k + seg / 2] = x - y;
+                omg = omg * omgn;
+            }
+        }
+    }
+    if (inv == -1) {
+        for (int i=0; i<len; ++i) 
+            F[i].rel /= len;
+    }
+}
+
+void convolution(vector<Complex> &a, vector<Complex> &b) {
+    FFT(a, 1); FFT(b, 1);
+    for (int i=0; i<a.size(); ++i) 
+        a[i] = a[i] * b[i];
+    FFT(a, -1); // "a" stores the product
+}
+
+void solve() {
+    cin >> s1 >> s2;
+    int len = 1, mx = max(s1.size(), s2.size());
+    while (len < (mx << 1)) len <<= 1;
+    vector<Complex> p1(len), p2(len);
+
+    for (int i=0; i<s1.size(); ++i) {
+        p1[i] = Complex(s1[s1.size() - i - 1] - '0', 0.0);
+    }
+    for (int i=0; i<s2.size(); ++i) {
+        p2[i] = Complex(s2[s2.size() - i - 1] - '0', 0.0);
+    }
+
+    convolution(p1, p2);
+    vector<int> res(len + 1);
+    for (int i=0; i<len; ++i) 
+        res[i] = p1[i].rel + 0.5;
+
+    for (int i=0; i<len; ++i) {
+        res[i + 1] += res[i] / 10;
+        res[i] %= 10;
+    }
+    auto it = res.rbegin();
+    while (*it == 0) ++it;
+    for (; it!=res.rend(); ++it) cout << *it;
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ Algorithm/Math/FFT/ntt.cpp
@@ -0,0 +1,124 @@
+/*
+***Math*** -- Number Theoretic Transformation (NTT)
+    Note:
+        這裡的應用是大數乘法。在整數體下的 FFT；見 ./fft.cpp
+    Time Complexity:
+        transformation: O(nlogn)
+        multiplication: O(n) (point-wise)
+    Reference:
+        https://blog.csdn.net/acdreamers/article/details/39005227
+*/
+#include <bits/stdc++.h>
+#define io cin.tie(NULL), ios_base::sync_with_stdio(false)
+using namespace std;
+using ll = long long;
+
+const int POW = 23; // N = 1 << POW (多項式長度)
+const int P = (15 << 27) + 1;
+const int G = 31; // generator (primitive root)
+
+vector<ll> omgn(POW + 1);
+string s1, s2;
+
+ll fpow(ll x, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) {
+            res *= x;
+            res %= P;
+        }
+        x *= x;
+        x %= P;
+        n >>= 1;
+    }
+    return res;
+}
+
+void getOmgn() {
+    for (int i=0; i<=POW; ++i) {
+        int t = 1 << i;
+        omgn[i] = fpow(G, (P-1) / t);
+    }
+}
+
+void rader(vector<ll> &F) {
+    int len = F.size();
+    int j = len >> 1;
+    for (int i=1; i<len-1; ++i) {
+        if (i < j) swap(F[i], F[j]);
+        int k = len >> 1;
+        while (j >= k) {
+            j -= k;
+            k >>= 1;
+        }
+        if (j < k) j += k;
+    }
+}
+
+void NTT(vector<ll> &F, int inv) {
+    rader(F);
+    int len = F.size(), at = 1;
+    for (int seg=2; seg<=len; seg<<=1, ++at) {
+        for (int i=0; i<len; i+=seg) {
+            ll omg = 1;
+            for (int j=i; j<i+seg/2; ++j) {
+                ll u = F[j] % P;
+                ll t = omg * F[j + seg/2] % P;
+                F[j] = (u + t) % P;
+                F[j + seg/2] = (u - t + P) % P;
+                omg = omg * omgn[at] % P;
+            }
+        }
+    }
+    if (inv == -1) {
+        for (int i=1; i<len/2; ++i) 
+            swap(F[i], F[len - i]);
+        ll inv = fpow(len, P - 2);
+        for (int i=0; i<len; ++i) 
+            F[i] = F[i] * inv % P;
+    }
+}
+
+void convolution(vector<ll> &a, vector<ll> &b) {
+    NTT(a, 1); NTT(b, 1);
+    for (int i=0; i<a.size(); ++i)
+        a[i] = a[i] * b[i] % P;
+    NTT(a, -1);
+}
+
+void solve() {
+    cin >> s1 >> s2;
+    int len = 1, mx = max(s1.size(), s2.size());
+    while (len < (mx << 1)) len <<= 1;
+    // cout << len << '\n';
+    vector<ll> p1(len), p2(len);
+
+    for (int i=0; i<s1.size(); ++i) {
+        p1[i] = s1[s1.size() - i - 1] - '0';
+    }
+    for (int i=0; i<s2.size(); ++i) {
+        p2[i] = s2[s2.size() - i - 1] - '0';
+    }
+
+    convolution(p1, p2);
+    vector<ll> res(p1);
+
+    for (int i=0; i<len; ++i) {
+        res[i + 1] += res[i] / 10;
+        res[i] %= 10;
+    }
+    auto it = res.rbegin();
+    while (*it == 0) ++it;
+    for (; it!=res.rend(); ++it) cout << *it;
+    cout << '\n';
+}
+
+int main() {
+    io;
+    getOmgn();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
