e142d9b8f4654b0d78576e4ddcfde3cbb2d5b370
lbwei <0800680274united@gmail.com>
2025-04-22 23:20:18 +0800

    feat: update decorator tutorial

--- python/Concept/decorator.py
+++ python/Concept/decorator.py
@@ -1,13 +1,44 @@
 # See https://openhome.cc/zh-tw/python/meta-programming/decorator/
+
+"""
+The return value of `first` in the below implementation is `None`, 
+and thus after calling the decorator `first(second)`, `second` is assigned
+with `None`, which is not callable.
+"""
+# def first(second):
+#     def wrapper(second):
+#         print('wrapper called')
+#         return second()
+    
+#     print('first function called')
+    
+#     return wrapper(second)
+
+"""
+The below implementation only calls `first()` initially.
+The wrapper and the `second` function are called when
+`second()` is explicitly executed.
+"""
 def first(second):
-    def wrapper(second):
+    """First function"""
+    def wrapper():
+        """Wrapper"""
         print('wrapper called')
         return second()
     
     print('first function called')
     
-    return wrapper(second)
+    return wrapper
 
+"""
+This "decorator on a function", is essentially:
+    second = first(second)
+"""
 @first
 def second():
     print('second function called')
+
+
+if __name__ == "__main__":
+    print(second.__doc__)
+    second()
